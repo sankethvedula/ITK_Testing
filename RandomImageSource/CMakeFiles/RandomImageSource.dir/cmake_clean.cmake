ff3ef9e7d69db50e1dc28b6615af1a7919dd1061
sankethvedula <sankethsai97@gmail.com>
2015-03-22 16:55:46 +0530

    Creating an Image with Random pixel values

---
+++ RandomImageSource/CMakeFiles/RandomImageSource.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/RandomImageSource.dir/RandomImageSource.cpp.o"
+  "RandomImageSource.pdb"
+  "RandomImageSource"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/RandomImageSource.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
