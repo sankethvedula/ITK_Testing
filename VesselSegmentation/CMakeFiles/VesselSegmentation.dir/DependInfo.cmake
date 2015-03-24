b70c040f5d76d3289fe78053245ab8fbfd9dd671
sankethvedula <sankethsai97@gmail.com>
2015-03-24 07:28:33 +0530

    Segmentation of blood vessels

---
+++ VesselSegmentation/CMakeFiles/VesselSegmentation.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/sanketh/Downloads/ITK_Testing/VesselSegmentation/VesselSegmentation.cpp" "/home/sanketh/Downloads/ITK_Testing/VesselSegmentation/CMakeFiles/VesselSegmentation.dir/VesselSegmentation.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+SET(CMAKE_TARGET_DEFINITIONS
+  "ITK_IO_FACTORY_REGISTER_MANAGER"
+  "vtkRenderingContext2D_AUTOINIT=1(vtkRenderingContextOpenGL)"
+  "vtkRenderingCore_AUTOINIT=3(vtkInteractionStyle,vtkRenderingFreeType,vtkRenderingOpenGL)"
+  "vtkRenderingCore_AUTOINIT=4(vtkInteractionStyle,vtkRenderingFreeType,vtkRenderingFreeTypeOpenGL,vtkRenderingOpenGL)"
+  "vtkRenderingVolume_AUTOINIT=1(vtkRenderingVolumeOpenGL)"
+  )
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "ITKIOFactoryRegistration"
+  "/usr/local/include/vtk-6.2"
+  "/usr/local/include/ITK-4.7"
+  "/usr/local/include"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ VesselSegmentation/CMakeFiles/VesselSegmentation.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/VesselSegmentation.dir/VesselSegmentation.cpp.o"
+  "VesselSegmentation.pdb"
+  "VesselSegmentation"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/VesselSegmentation.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
